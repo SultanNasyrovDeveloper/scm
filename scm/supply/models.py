342d35d5c72a4717592082401687dca5e66255cc
shumrussia <azat.nuriakhmetov@gmail.com>
2022-06-24 17:03:54 +0300

    styling issues

--- scm/supply/models.py
+++ scm/supply/models.py
@@ -1,30 +1,34 @@
 from django.db import models
+
 from .enums import POStatus
+
 
 class PurchaseOrder(models.Model):
     """
     Represents an instance of a purhase order for a particular SKU or a set of SKUs
     """
+
     status = models.CharField(
         max_length=50,
         choices=POStatus.choices,
         default=POStatus.created
     )
-
     created_datetime = models.DateTimeField(auto_now=True)
     estimated_delivery_datetime = models.DateTimeField()
     actual_delivery_datetime = models.DateTimeField()
     vendor = models.ForeignKey(
         'supply.Vendor',
         related_name='purchase_orders',
-        null=False
+        null=False,
+        on_delete=models.DO_NOTHING
     )
-    
+
 
 class PurchaseItem(models.Model):
     """
     Represents an item of a purchase order
     """
+
     purchase_order = models.ForeignKey(
         'supply.PurchaseOrder',
         on_delete=models.CASCADE,
@@ -32,9 +36,11 @@ class PurchaseItem(models.Model):
     )
     sku = models.ForeignKey(
         'sku.SKU',
+        on_delete=models.DO_NOTHING
     )
     purchased_quantity = models.DecimalField()
     delivered_quantity = models.DecimalField()
+
 
 class Vendor(models.Model):
     """
@@ -45,5 +51,4 @@ class Vendor(models.Model):
         max_length=255,
         default = ''
     )
-
     default_transit_time_ms = models.IntegerField(null=False)
